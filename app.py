3c2ba2ae43299e263ffcbb51874dcf4e956efefe
FlavienMedina <flavien_94@hotmail.fr>
2023-10-09 16:12:56 +0200

    [PATCH] Request Client to API

--- app.py
+++ app.py
@@ -5,8 +5,27 @@ from flask import Flask, render_template
 
 from src import load_template, query_database
 
+app = Flask(__name__)
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/get_vehicules/')
+def get_vehicules():
+    print('pomme')
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    res = cur.execute("SELECT * FROM vehicule")
+    results = res.fetchall()
+    print(results)
+    data = [{"id": id_, "desc": name} for id_, name in results]
+    con.close()
+    print(data)
+    return jsonify(data)
 
 @app.route('/get_summary/<int:id>')
 def generate_pdf_summary_file(id: int):
@@ -21,3 +40,6 @@ def generate_pdf_summary_file(id: int):
     print(response)
     con.close()
     return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- client/script.js
+++ client/script.js
@@ -1,16 +1,38 @@
-const API_URL = 'https://your-api-endpoint.com/vehicles';
+const API_URL = 'http://127.0.0.1:5000';
 const vehicleDropdown = document.getElementById('vehicleDropdown');
+const vehicleForm = document.getElementById('vehicleForm');
 
-fetch(API_URL)
+fetch(API_URL + '/get_vehicules')
     .then(response => response.json())
     .then(data => {
+        console.log(data);
         data.forEach(vehicle => {
             const option = document.createElement('option');
             option.value = vehicle.id;
-            option.textContent = vehicle.name;
-            vehicleDropdown.appendChild
+            option.textContent = vehicle.desc;
+            vehicleDropdown.appendChild(option);
         });
     })
     .catch(error => {
         console.error("Erreur lors de la récupération des véhicules:", error);
     });
+
+vehicleForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const selectedVehicleId = vehicleDropdown.value;
+
+    fetch(`${API_URL}/get_summary/${selectedVehicleId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Réponse du serveur:', data);
+        // Traitez la réponse ici, par exemple en affichant un message à l'utilisateur
+    })
+    .catch(error => {
+        console.error('Erreur lors de la soumission:', error);
+    });
+});
