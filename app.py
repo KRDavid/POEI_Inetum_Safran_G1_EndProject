2c0da366a2bb743dae716d09e9c5610dc4fb1098
David K <drakkix@gmail.com>
2023-10-09 16:18:14 +0200

    [UPDATE] fixed dirty copy paste from this enculé named Flavien

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
 import sqlite3
 import json
 
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
 
 from src import load_template, query_database
 
-app = Flask(__name__)
 
-from flask import Flask, jsonify
-from flask_cors import CORS
-import sqlite3
 
 app = Flask(__name__)
 CORS(app)
 
+
+
 @app.route('/get_vehicules/')
 def get_vehicules():
-    print('pomme')
     con = sqlite3.connect("database.db")
     cur = con.cursor()
+
     res = cur.execute("SELECT * FROM vehicule")
     results = res.fetchall()
-    print(results)
+
     data = [{"id": id_, "desc": name} for id_, name in results]
+
     con.close()
-    print(data)
+
     return jsonify(data)
+
+
 
 @app.route('/get_summary/<int:id>')
 def generate_pdf_summary_file(id: int):
@@ -35,9 +37,7 @@ def generate_pdf_summary_file(id: int):
 
     data = query_database.get_datas(cur, id)
 
-    print(data)
     response = json.dumps(data)
-    print(response)
     con.close()
     return response
 
