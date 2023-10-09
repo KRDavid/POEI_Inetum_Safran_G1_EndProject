5638940f1a051e279ecac83e76811b23d0f8f63a
David K <drakkix@gmail.com>
2023-10-09 12:00:22 +0200

    [UPDATE] updated app.py to connect to db with simple query

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask
+import sqlite3
+import json
 
 app = Flask(__name__)
 
+
+
+
 @app.route('/get_summary/<int:id>')
 def generate_pdf_summary_file(id: int):
-    return f"id: {id}"
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    res = cur.execute("SELECT * FROM vehicule WHERE vehicule_id = ?", [str(id)])
+    data = res.fetchall()
+    print(data)
+    response = json.dumps(data)
+    print(response)
+    con.close()
+    return response
 
