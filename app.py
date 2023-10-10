598d6537beb1d5848444f84ba63fe6590491812f
FlavienMedina <flavien_94@hotmail.fr>
2023-10-10 14:00:05 +0200

    [ADD] Download Routes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sqlite3
 import json
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 
 from src import load_template, query_database
@@ -24,8 +24,6 @@ def get_vehicules():
 
     return jsonify(data)
 
-
-
 @app.route('/get_summary/<int:id>')
 def generate_pdf_summary_file(id: int):
 
@@ -36,8 +34,13 @@ def generate_pdf_summary_file(id: int):
     response = json.dumps(data)
     con.close()
 
-    load_template.create_pdf(data)
-    return response
+    download = load_template.create_pdf(data)
+    return download
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory('output', f"{filename}.pdf", as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
