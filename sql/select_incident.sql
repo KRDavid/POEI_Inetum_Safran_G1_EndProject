207196c908c222e2b0620ac82f4df7ac2562f2fa
David K <drakkix@gmail.com>
2023-10-10 10:48:26 +0200

    [FIX] fixed SQL query for posts table

--- sql/select_incident.sql
+++
@@ -1,6 +0,0 @@
--- Selelectionne l'identifiant d'un incident et l'ordre de travail lié
--- Retourne incident_id et ordre
-SELECT "incident"."incident_id", "ordre"."ordre_id"
-FROM "incident"
-LEFT JOIN "ordre" ON "ordre"."ordre_id" = "incident"."ordre"
-WHERE "ordre"."vehicule" = ?;
--- src/load_template.py
+++ src/load_template.py
@@ -1,11 +1,13 @@
 import jinja2
 
-templateLoader = jinja2.FileSystemLoader(searchpath="./")
-templateEnv = jinja2.Environment(loader=templateLoader)
-TEMPLATE_FILE = "./template/template.html"
-template = templateEnv.get_template(TEMPLATE_FILE)
-outputText = template.render(vehicule={"desc": 'Jordan t es mort'})
 
-html_file = open('test.html', 'w', encoding="utf-8")
-html_file.write(outputText)
-html_file.close()
+def load_template(data):
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = "./template/template.html"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    outputText = template.render(vehicule={"desc": 'Jordan t es mort'})
+
+    html_file = open('test.html', 'w', encoding="utf-8")
+    html_file.write(outputText)
+    html_file.close()
