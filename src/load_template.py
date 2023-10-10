67ab1f2a3a524996be7066865396f0988447c3cb
FlavienMedina <flavien_94@hotmail.fr>
2023-10-10 10:50:31 +0200

    [ADD] Create PDF Function

--- src/load_template.py
+++ src/load_template.py
@@ -1,11 +1,24 @@
-import jinja2
+from jinja2 import Environment, FileSystemLoader
+import pdfkit
 
-templateLoader = jinja2.FileSystemLoader(searchpath="./")
-templateEnv = jinja2.Environment(loader=templateLoader)
-TEMPLATE_FILE = "./template/template.html"
-template = templateEnv.get_template(TEMPLATE_FILE)
-outputText = template.render(vehicule={"desc": 'Jordan t es mort'})
+def create_pdf(data):
+    templateLoader = FileSystemLoader(searchpath="./")
+    templateEnv = Environment(loader=templateLoader)
+    TEMPLATE_FILE = "./template/template.html"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    outputText = template.render(vehicule=data)
 
-html_file = open('test.html', 'w', encoding="utf-8")
-html_file.write(outputText)
-html_file.close()
+    html_file = open('output.html', 'w', encoding="utf-8")
+    html_file.write(outputText)
+    html_file.close()
+
+    options = {
+        'header-html': './header.html',
+        'footer-right': '[page]',
+        'margin-top': '50mm',
+        'margin-bottom': '30mm',
+        'no-outline': None,
+        'allow': ['./logo.jpg',],
+    }
+
+    pdfkit.from_file('output.html', 'output.pdf', options=options)
